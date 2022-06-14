dae566d77f2c138f1819eff9f1d396a71d66c3cb
jeronimoespora <jeronimo.espora@hotmail.com>
2022-06-14 19:01:03 -0300

    adding
    guess vs computer
    
    rock_paper_scissors
    
    hangman

--- Guess.py
+++ Guess.py
@@ -1,12 +1,12 @@
 import random
 def guess(x):
     random_number = random.randint(1,x)
-    guess = 0
-    while guess != random_number:
-        guess = int(input(f'Guess a number between 1 and {x} '))
-        if guess < random_number:
+    guess2 = 0
+    while guess2 != random_number:
+        guess2 = int(input(f'Guess a number between 1 and {x} '))
+        if guess2 < random_number:
             print('Sorry, guess again. Too low.')
-        elif guess > random_number:
+        elif guess2 > random_number:
             print('Sorry, guess again. Too high.')
 
     print('Yay, congrats. You have guessed the number',random_number,'correctly!')
