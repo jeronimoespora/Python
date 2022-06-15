c7cd626ec230c73bb929ffd46be5dd83ad42b818
jeronimoespora <jeronimo.espora@hotmail.com>
2022-06-15 01:41:09 -0300

    organization
    add folders
    
    add tic tac toe (unfinished)

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,6 +1,6 @@
 import random
-from words import words
-from hangman_visual import lives_visual_dict
+from hangman.words import words
+from hangman.hangman_visual import lives_visual_dict
 import string
 
 
