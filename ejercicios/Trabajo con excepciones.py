fed03b6915898d00fe89e813e8e3b4ce5922bfdb
jeronimoespora <jeronimo.espora@hotmail.com>
2022-07-10 16:52:39 -0300

    por revisar

---
+++ ejercicios/Trabajo con excepciones.py
@@ -0,0 +1,54 @@
+import sys
+
+def suma(num1, num2):
+	return num1+num2
+
+def resta(num1, num2):
+	return num1-num2
+
+def multiplica(num1, num2):
+	return num1*num2
+
+def divide(num1,num2):		
+	try:
+		return num1/num2
+	except ZeroDivisionError:
+		print("No se puede dividir entre 0")
+		return "Operacion errónea"
+	
+contador = 0
+while True:
+	try:
+		op1=(int(input("Introduce el primer número: ")))
+
+		op2=(int(input("Introduce el segundo número: ")))	
+
+		break
+
+	except ValueError:
+		contador +=1
+		print("Los Vvalores introducidos no son correctos")	
+		if contador == 3:
+			sys.exit()
+
+	
+operacion=input("Introduce la operación a realizar (suma,resta,multiplica,divide): ")
+
+if operacion=="suma":
+	print(suma(op1,op2))
+
+elif operacion=="resta":
+	print(resta(op1,op2))
+
+elif operacion=="multiplica":
+	print(multiplica(op1,op2))
+
+elif operacion=="divide":
+	print(divide(op1,op2))
+	
+
+else:
+	print ("Operación no contemplada")
+
+
+print("Operación ejecutada. Continuación de ejecución del programa ")
