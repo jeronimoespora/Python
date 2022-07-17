0cf4d32c8d0ed96daa435260d1186ff081cc50fd
jeronimoespora <jeronimo.espora@hotmail.com>
2022-07-16 21:06:29 -0300

    Create classes.py

---
+++ ejercicios/pildorasinformaticas/classes.py
@@ -0,0 +1,15 @@
+class CuentaCorriente:
+    numeroCuenta = "361273"
+    titularCuenta = "Carlos Salvador"
+    saldoCuenta = 1500
+
+    def showInfo(self):
+        print(str(numeroCuenta))
+        print(str(titularCuenta))
+        print(str(saldoCuenta))
+
+    def deposit(self):
+        pass
+
+    def extract(self):
+        pass
