14cb7f370a225ddff0d55a84732c058f2ad4c86a
jeronimoespora <jeronimo.espora@hotmail.com>
2022-07-22 19:53:33 -0300

    Update classes.py
    trabajando con clases

--- ejercicios/pildorasinformaticas/classes.py
+++ ejercicios/pildorasinformaticas/classes.py
@@ -1,15 +1,25 @@
 class CuentaCorriente:
-    numeroCuenta = "361273"
-    titularCuenta = "Carlos Salvador"
-    saldoCuenta = 1500
+    def __init__(self, numeroCuenta, titularCuenta, saldoCuenta):
+        self.numeroCuenta = numeroCuenta
+        self.titularCuenta = titularCuenta
+        self.saldoCuenta = saldoCuenta
 
-    def showInfo(self):
-        print(str(numeroCuenta))
-        print(str(titularCuenta))
-        print(str(saldoCuenta))
+    def getDatos(self):
+        return ("El titular de la cuenta es: "+ self.titularCuenta +". Su numero de cuenta es: "+
+            self.numeroCuenta + \
+            ". Y su saldo actual es:  " + str(self.saldoCuenta)
+        )
 
-    def deposit(self):
-        pass
+    def deposit(self, toDepost):
+        self.saldoCuenta = self.saldoCuenta + toDepost
 
-    def extract(self):
-        pass
+    def extract(self, toExtract):
+        self.saldoCuenta = self.saldoCuenta - toExtract
+
+
+cuenta1 = CuentaCorriente("153846", "Pedro Longoria", 1500)
+print(cuenta1.getDatos())
+cuenta1.deposit(1500)
+print(cuenta1.getDatos())
+cuenta1.extract(1000)
+print(cuenta1.getDatos())
