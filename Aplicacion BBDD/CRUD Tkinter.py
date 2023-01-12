afbc40c4581090ebf207d7c6cb3e3999cb881c5c
jeronimoespora <jeronimo.espora@hotmail.com>
2023-01-12 17:28:32 -0300

    Update CRUD Tkinter.py
    interfaz actualizada.
    terminada.

--- Aplicacion BBDD/CRUD Tkinter.py
+++ Aplicacion BBDD/CRUD Tkinter.py
@@ -20,9 +20,79 @@ crudMenu.add_command(label="leer")
 crudMenu.add_command(label="Actualizar")
 crudMenu.add_command(label="Borrar")
 
-borrarMenu = Menu(barraMenu, tearoff=0)
+ayudaMenu = Menu(barraMenu, tearoff=0)
 ayudaMenu.add_command(label="Licencia")
-ayudaenu.add_command(label="Acerca de...")
+ayudaMenu.add_command(label="Acerca de...")
+
+barraMenu.add_cascade(label="BBDD", menu=bbddMenu)
+barraMenu.add_cascade(label="Borrar", menu=borrarMenu)
+barraMenu.add_cascade(label="CRUD", menu=crudMenu)
+barraMenu.add_cascade(label="Ayuda", menu=ayudaMenu)
+
+# -------------------------------construccion campos grid------------------------------
+
+miFrame = Frame(root)
+miFrame.pack()
+
+cuadroId = Entry(miFrame)
+cuadroId.grid(row=0, column=1, padx=10, pady=10)
+
+cuadroNombre = Entry(miFrame)
+cuadroNombre.grid(row=1, column=1, padx=10, pady=10)
+cuadroNombre.config(fg="red", justify="right")
+
+cuadroPass = Entry(miFrame)
+cuadroPass.grid(row=2, column=1, padx=10, pady=10)
+cuadroPass.config(show="*")
+
+cuadroApellido = Entry(miFrame)
+cuadroApellido.grid(row=3, column=1, padx=10, pady=10)
+
+cuadroDireccion = Entry(miFrame)
+cuadroDireccion.grid(row=4, column=1, padx=10, pady=10)
+
+textComentario = Text(miFrame, width=16, height=5)
+textComentario.grid(row=5, column=1, padx=10, pady=10)
+scrollVert = Scrollbar(miFrame, command=textComentario.yview)
+scrollVert.grid(row=5, column=2, sticky="nsew")
+textComentario.config(yscrollcommand=scrollVert.set)
+
+# -------------------------Creación de Labels--------------------------
+
+idLabel = Label(miFrame, text="Id:")
+idLabel.grid(row=0, column=0, sticky="e", padx=10, pady=10)
+
+nombreLabel = Label(miFrame, text="Nombre:")
+nombreLabel.grid(row=1, column=0, sticky="e", padx=10, pady=10)
+
+passLabel = Label(miFrame, text="Contraseña:")
+passLabel.grid(row=2, column=0, sticky="e", padx=10, pady=10)
+
+apellidoLabel = Label(miFrame, text="Apellido:")
+apellidoLabel.grid(row=3, column=0, sticky="e", padx=10, pady=10)
+
+direccionLabel = Label(miFrame, text="Direccion:")
+direccionLabel.grid(row=4, column=0, sticky="e", padx=10, pady=10)
+
+comentariosLabel = Label(miFrame, text="Comentarios:")
+comentariosLabel.grid(row=5, column=0, sticky="e", padx=10, pady=10)
+
+# ------------------------- colocación de botones-----------------
+
+miFrameBotones = Frame(root)
+miFrameBotones.pack()
+
+botonCrear = Button(miFrameBotones, text="Crear")
+botonCrear.grid(row=1, column=0, sticky="e", padx=10, pady=10)
+
+botonLeer = Button(miFrameBotones, text="Leer")
+botonLeer.grid(row=1, column=1, sticky="e", padx=10, pady=10)
+
+botonActualizar = Button(miFrameBotones, text="Actualizar")
+botonActualizar.grid(row=1, column=2, sticky="e", padx=10, pady=10)
+
+botonBorrar = Button(miFrameBotones, text="Borrar")
+botonBorrar.grid(row=1, column=3, sticky="e", padx=10, pady=10)
 
 
-root.mainloop
+root.mainloop()
