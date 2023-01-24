22316afaaa50cf89a141397dfb24260a320ef56d
jeronimoespora <jeronimo.espora@hotmail.com>
2023-01-24 14:59:31 -0300

    Reorganización
    -reorganice repositorios
    - empecé a trabajar con flask

--- Calculadora/calculadoraModular/resultados_pantalla.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-
-def mostrar_pantalla(self, valor):
-    self.display.insert(END, valor)
-
-
-def borrar_pantalla(self):
-    self.display.delete(0, END)
--- Flask/app.py
+++ Flask/app.py
@@ -27,6 +27,19 @@ def usuarios(numusuario):
     return "Bienvenido a la web. Usuario nº {} ".format(numusuario)
 
 
+@app.route("/datosusuario/<int:id>/<string:nombreusuario>")
+def datosusuario(id, nombreusuario):
+    return "Estos son los datos del usuario. Id: {}. Nombre de usuario: {}".format(
+        id, nombreusuario
+    )
+
+
+@app.route("/posts")
+@app.route("/posts/<int:npost>")
+def posts(npost=0):
+    return "Este es el post nº {}".format(npost)
+
+
 if __name__ == "__main__":
 
     os.environ["FLASK_ENV"] = "development"
